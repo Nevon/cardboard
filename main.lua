1743aa5e65ebdc6d44aead6423e2a6d0bd962d44
Bart van Strien <bart@bartbes.ath.cx>
2012-05-19 17:31:16 +0200

    Add networking?

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
 Class = require "lib.class"
+require "lib.LUBE"
+
+local server = true
+local conn
 
 Card = Class{function(self, id)
 	self.id = id
@@ -25,14 +29,14 @@ Card = Class{function(self, id)
 	elseif self.value == 13 then self.name = "King of "..self.suit
 	elseif self.value == 14 then self.name = "Ace of "..self.suit end
 
-	self.image = love.graphics.newImage("images/cards/"..self.suit.."-"..self.value.."-75.png");
+	self.image = love.graphics.newImage("images/cards/"..self.suit.."-"..self.value.."-75.png")
 end}
 
 function Card:draw()
 	love.graphics.draw(self.image, self.x, self.y);
 end
 
-function Card:moved(x,y)
+function Card:moved(x, y, remote)
 	print (self.name .. " (" .. self.id .. ") " .. "was moved to " .. x .. "/" .. y)
 
 	self.x = x
@@ -50,6 +54,10 @@ function Card:moved(x,y)
 	end
 
 	table.insert(deck, thisCard)
+
+	if not remote then
+		conn:send(("moved:%d:%d:%d"):format(self.id, x, y))
+	end
 end
 
 function Card:clicked(x, y)
@@ -122,7 +130,124 @@ do
     end
 end
 
-function love.load()
+local function serverRecv(data, clientid)
+	if data == "getDeck" then
+		for i = 1, #deck do
+			conn:send(
+				("%d:%d:%d\n"):format(deck[i].id, deck[i].x, deck[i].y),
+				clientid)
+		end
+	elseif data:match("^moved:") then
+		local id, x, y = data:match("^moved:(%d+):(%d+):(%d+)$")
+		assert(id, "Invalid message")
+		id, x, y = tonumber(id), tonumber(x), tonumber(y)
+		for i, v in ipairs(deck) do
+			if v.id == id then
+				v:moved(x, y, true)
+				break
+			end
+		end
+	end
+end
+
+local function clientRecv(data)
+	if data:match("^moved:") then
+		local id, x, y = data:match("^moved:(%d+):(%d+):(%d+)$")
+		assert(id, "Invalid message")
+		id, x, y = tonumber(id), tonumber(x), tonumber(y)
+		for i, v in ipairs(deck) do
+			if v.id == id then
+				v:moved(x, y, true)
+				break
+			end
+		end
+	end
+end
+
+local function prepareNetwork(args)
+	if args[1] == "client" then
+		server = false
+		table.remove(args, 1)
+	else
+		if args[1] == "server" then
+			table.remove(args, 1)
+		else
+			print("Invalid mode, defaulting to server")
+		end
+		server = true
+	end
+
+	if server then
+		conn = lube.tcpServer()
+		conn.handshake = "helloCardboard"
+		conn:listen(3410)
+		conn.callbacks.recv = serverRecv
+	else
+		local host = args[1]
+		if not host then
+			print("Invalid host, defaulting to localhost")
+			host = "localhost"
+		end
+		conn = lube.tcpClient()
+		conn.handshake = "helloCardboard"
+		assert(conn:connect(host, 3410, true))
+		conn.callbacks.recv = clientRecv
+	end
+end
+
+local getLine
+do
+	local msg
+	local it
+
+	local function getMsg()
+		repeat
+			msg = conn:receive()
+			love.timer.sleep(0.005)
+		until msg
+	end
+
+	function getLine()
+		if not msg then
+			getMsg()
+			it = msg:gmatch("[^\n]+")
+		end
+		local line = it()
+		if not line then
+			msg = nil
+			return getLine()
+		end
+		return line
+	end
+end
+
+local function prepareDeck()
+	if server then
+		--Shuffle the shit out of it
+		for i=#deck, 1, -1 do
+			local toMove = math.random(i)
+			deck[toMove], deck[i] = deck[i], deck[toMove]
+		end
+	else
+		local msg, line, id, x, y
+		conn:send("getDeck")
+		for i = 1, #deck do
+			repeat
+				local line = getLine()
+				id, x, y = line:match("(%d+):(%d+):(%d+)")
+				if not id then
+					clientRecv(msg)
+				end
+			until id and x and y
+			id, x, y = tonumber(id), tonumber(x), tonumber(y)
+			deck[i]:construct(id)
+			deck[i].x, deck[i].y = x, y
+			id, x, y = nil, nil, nil
+		end
+	end
+end
+
+function love.load(args)
 	math.randomseed(os.time())
 	math.random()
 	math.random()
@@ -138,22 +263,23 @@ function love.load()
 
 	love.audio.play('sounds/shuffle.ogg')
 
-	--Shuffle the shit out of it
-	for i=#deck, 1, -1 do
-		local toMove = math.random(i)
-		deck[toMove], deck[i] = deck[i], deck[toMove]
+	local nwArgs = {}
+	for i = 2, #args do
+		table.insert(nwArgs, args[i])
 	end
+	prepareNetwork(nwArgs)
+	prepareDeck()
 end
 
 function love.update(dt)
-
+	conn:update(dt)
 end
 
 function love.draw()
-	love.graphics.draw(bg, 0, 0);
+	love.graphics.draw(bg, 0, 0)
 
 	for i,v in ipairs(deck) do
-		v:draw();
+		v:draw()
 	end
 end
 
